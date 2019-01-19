2df5f2b665b250499dd9294cfc1664f643a43f09
Jane Venter <janeanneventer@gmail.com>
2019-01-19 19:17:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,6 @@
 INSERT INTO bears (name) VALUES ("Mr. Chocolate");
 INSERT INTO bears (name) VALUES ("Rowdy");
+INSERT INTO bears (name) VALUES ("Tabitha")
 INSERT INTO bears (name) VALUES ("Sergeant Brown");
 INSERT INTO bears (name) VALUES ("Melissa");
 INSERT INTO bears (name) VALUES ("Grinch");
